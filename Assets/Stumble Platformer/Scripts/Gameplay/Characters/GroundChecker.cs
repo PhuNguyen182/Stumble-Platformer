b9333a6fa604df3680484d370ba8e1e739cb4572
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-10-29 23:40:10 +0700

    Add temp scenes

--- Assets/Stumble Platformer/Scripts/Gameplay/Characters/GroundChecker.cs
+++ Assets/Stumble Platformer/Scripts/Gameplay/Characters/GroundChecker.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,41 +11,14 @@ namespace StumblePlatformer.Scripts.Gameplay.Characters
         [SerializeField] private float checkGroundRadius;
 
         private Vector3 _checkPosition;
-        private Collider[] _groundResult;
 
         public bool IsGrounded { get; private set; }
         public Vector3 GroundVelocity { get; private set; }
-
-        private void Awake()
-        {
-            _groundResult = new Collider[20];
-        }
 
         private void Update()
         {
             _checkPosition = transform.position + groundOffset;
             IsGrounded = Physics.CheckSphere(_checkPosition, checkGroundRadius, groundMask);
-        }
-
-        private void InspectGround()
-        {
-            int resultCount = Physics.OverlapSphereNonAlloc(_checkPosition, checkGroundRadius, _groundResult, groundMask);
-
-            if (resultCount <= 0)
-            {
-                GroundVelocity = Vector3.zero;
-                return;
-            }
-
-            if (_groundResult[0].TryGetComponent<Rigidbody>(out var groundBody))
-            {
-                GroundVelocity = groundBody.velocity;
-            }
-        }
-
-        private void OnDestroy()
-        {
-            Array.Clear(_groundResult, 0, _groundResult.Length);
         }
     }
 }
