9b9fad745f61b18f60667e417ac224ed547672ef
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-11-04 19:15:29 +0700

    Remove unused assets

--- Assets/Stumble Platformer/Scripts/Gameplay/Game Entities/Obstacles/ObstacleAttacker.cs
+++ Assets/Stumble Platformer/Scripts/Gameplay/Game Entities/Obstacles/ObstacleAttacker.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 
 namespace StumblePlatformer.Scripts.Gameplay.GameEntities.Obstacles
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class ObstacleAttacker : MonoBehaviour
     {
+        [SerializeField] private bool canFreeCheckBodyType;
+
         public bool CanAttack { get; set; }
+
+#if UNITY_EDITOR
+        private Rigidbody _attactBody;
+
+        private void OnValidate()
+        {
+            _attactBody ??= GetComponent<Rigidbody>();
+
+            if (_attactBody != null && !canFreeCheckBodyType)
+                _attactBody.isKinematic = true;
+
+        }
+#endif
     }
 }
