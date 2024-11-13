363c7d7395c0866824a5314e6b8b9bbf5026d251
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-11-13 22:52:38 +0700

    Add finish zone

---
+++ Assets/Stumble Platformer/Scripts/Gameplay/Game Entities/Level Platforms/FinishZone.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StumblePlatformer.Scripts.Gameplay.GameEntities.LevelPlatforms
+{
+    public class FinishZone : MonoBehaviour
+    {
+
+    }
+}
