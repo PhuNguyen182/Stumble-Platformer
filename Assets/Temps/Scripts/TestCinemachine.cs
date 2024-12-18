01524b75f542dff9422b8a2c7129ad8f194cbdc3
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-12-19 00:23:03 +0700

    Add laser shader and materials

--- Assets/Temps/Scripts/TestCinemachine.cs
+++ Assets/Temps/Scripts/TestCinemachine.cs
@@ -6,9 +6,12 @@ using Cinemachine;
 public class TestCinemachine : MonoBehaviour
 {
     public CinemachineVirtualCamera virtualCamera;
+    public Material mat;
+    public Color color;
 
     private void Start()
     {
+        color = mat.GetColor("_Color");
         virtualCamera.DestroyCinemachineComponent<CinemachineTransposer>();
     }
 }
