76c721a634d4d196d522f65053c788fb4cbaf6ee
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-12-23 11:39:17 +0700

    Update level 26

--- Assets/Stumble Platformer/Scripts/Gameplay/Game Entities/Platforms/RotatePlatform.cs
+++ Assets/Stumble Platformer/Scripts/Gameplay/Game Entities/Platforms/RotatePlatform.cs
@@ -45,17 +45,17 @@ namespace StumblePlatformer.Scripts.Gameplay.GameEntities.Platforms
 
         public override void OnPlatformCollide(Collision collision)
         {
-            _hasPlayerStandOn = collision.HasLayer(playerMask);
+            
         }
 
         public override void OnPlatformStay(Collision collision)
         {
-            _hasPlayerStandOn = collision.HasLayer(playerMask);
+            
         }
 
         public override void OnPlatformExit(Collision collision)
         {
-            _hasPlayerStandOn = false;
+            
         }
 
         public override void PlatformAction()
@@ -102,26 +102,26 @@ namespace StumblePlatformer.Scripts.Gameplay.GameEntities.Platforms
 
         private void OnPlatformTriggerEnter(Collider collider)
         {
-            if (!_hasPlayerStandOn)
-                return;
-
-            if (collider.TryGetComponent(out ICharacterParentSetter parentSetter))
+            if (!collider.HasLayer(playerMask))
             {
-                parentSetter.SetParent(transform);
+                if (collider.TryGetComponent(out ICharacterParentSetter parentSetter))
+                {
+                    parentSetter.SetParent(platformBody.transform);
+                }
             }
         }
 
         private void OnPlatformTriggerStay(Collider collider)
         {
-            if (!_hasPlayerStandOn)
+            if (collider.transform.parent != null && collider.transform.parent.GetInstanceID() != platformBody.transform.GetInstanceID())
                 return;
 
-            if (collider.transform.parent != null && collider.transform.parent.GetInstanceID() != this.GetInstanceID())
-                return;
-
-            if (collider.TryGetComponent(out ICharacterParentSetter parentSetter))
+            if (collider.HasLayer(playerMask))
             {
-                parentSetter.SetParent(transform);
+                if (collider.TryGetComponent(out ICharacterParentSetter parentSetter))
+                {
+                    parentSetter.SetParent(platformBody.transform);
+                }
             }
         }
 
