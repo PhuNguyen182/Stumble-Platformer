6fdbabd11d88848a1af025cf8bd4d18c0be46e16
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-12-24 16:33:43 +0700

    Update rotate obstacle, add temp level 28

--- Assets/Stumble Platformer/Scripts/Gameplay/Game Entities/Obstacles/RotateObstacle.cs
+++ Assets/Stumble Platformer/Scripts/Gameplay/Game Entities/Obstacles/RotateObstacle.cs
@@ -10,8 +10,16 @@ namespace StumblePlatformer.Scripts.Gameplay.GameEntities.Obstacles
     public class RotateObstacle : BaseObstacle
     {
         [SerializeField] private float rotateSpeed = 10f;
+        [SerializeField] private float invertTimer = 6f;
+        [SerializeField] private float nextInversionTimer = 3f;
         [SerializeField] private RotateAxis rotateAxis;
+        [SerializeField] private RotateMode rotateMode;
 
+        private float _rotateSpeed = 0;
+        private float _invertTimer = 0;
+        private float _rotateWeight = 1;
+        private float _nextInvertTime = 0;
+        private int _nextCount = 0;
         private Vector3 _rotateAxis;
 
         public override void OnAwake()
@@ -23,6 +31,8 @@ namespace StumblePlatformer.Scripts.Gameplay.GameEntities.Obstacles
                 RotateAxis.Z => Vector3.forward,
                 _ => Vector3.zero
             };
+            IsActive = true;
+            _rotateSpeed = rotateSpeed;
         }
 
         public override void DamageCharacter(Collision collision)
@@ -56,8 +66,63 @@ namespace StumblePlatformer.Scripts.Gameplay.GameEntities.Obstacles
 
         public override void ObstacleAction()
         {
-            Quaternion rotation = Quaternion.Euler(_rotateAxis * rotateSpeed * Time.fixedDeltaTime);
+            switch (rotateMode)
+            {
+                case RotateMode.SwitchWise:
+                    RotateSwitchWise();
+                    break;
+                case RotateMode.InvertGradually:
+                    InvertGradually();
+                    break;
+            }
+
+            RotateContinous();
+        }
+
+        private void RotateContinous()
+        {
+            Quaternion rotation = Quaternion.Euler(_rotateAxis * _rotateSpeed * _rotateWeight * Time.fixedDeltaTime);
             obstacleBody.MoveRotation(obstacleBody.rotation * rotation);
+        }
+
+        private void RotateSwitchWise()
+        {
+            _invertTimer += Time.deltaTime;
+            if (_invertTimer > invertTimer)
+            {
+                _invertTimer = 0;
+                _rotateSpeed *= -1;
+            }
+        }
+
+        private void InvertGradually()
+        {
+            if (_invertTimer < invertTimer)
+            {
+                _rotateWeight = 1;
+                _invertTimer += Time.deltaTime;
+            }
+
+            if(_invertTimer > invertTimer)
+            {
+                _nextInvertTime += Time.deltaTime;
+                _rotateWeight = _nextCount == 0 ? Mathf.Lerp(1f, 0f, _nextInvertTime / nextInversionTimer)
+                                                : Mathf.Lerp(0f, 1f, _nextInvertTime / nextInversionTimer);
+
+                if(_nextInvertTime > nextInversionTimer)
+                {
+                    _nextCount += 1;
+                    _nextInvertTime = 0;
+                    _rotateSpeed *= -1;
+
+                    if (_nextCount == 2)
+                    {
+                        _nextCount = 0;
+                        _rotateSpeed *= -1;
+                        _invertTimer = 0;
+                    }
+                }
+            }
         }
     }
 }
---
+++ Assets/Stumble Platformer/Scripts/Gameplay/Game Entities/Obstacles/SpiralSpinnerObstacle.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StumblePlatformer.Scripts.Gameplay.GameEntities.Characters;
+using StumblePlatformer.Scripts.Gameplay.GameEntities.Characters.Damageables;
+using StumblePlatformer.Scripts.Common.Enums;
+
+namespace StumblePlatformer.Scripts.Gameplay.GameEntities.Obstacles
+{
+    public class SpiralSpinnerObstacle : BaseObstacle
+    {
+        [Header("Spin Spiral")]
+        [SerializeField] private Vector3 spiralCenter;
+        [SerializeField] private float spiralRadiusTime = 6f;
+        [SerializeField] private float spiralRotateSpeed = 10f;
+        [SerializeField] private float minSpiralRadius = 1f;
+        [SerializeField] private float maxSpiralRadius = 6f;
+
+        [Header("Spin Self")]
+        [SerializeField] private float selfRotateSpeed = 10f;
+        [SerializeField] private RotateAxis rotateAxis;
+
+        private float _spiralTime = 0;
+        private Vector3 _rotateAxis;
+        private Vector3 _originalPosition;
+
+        public override void OnAwake()
+        {
+            _rotateAxis = rotateAxis switch
+            {
+                RotateAxis.X => Vector3.right,
+                RotateAxis.Y => Vector3.up,
+                RotateAxis.Z => Vector3.forward,
+                _ => Vector3.zero
+            };
+
+            _originalPosition = transform.position;
+        }
+
+        public override void DamageCharacter(Collision collision)
+        {
+            if (!collision.transform.TryGetComponent(out ICharacterMovement characterMovement))
+                return;
+
+            if (collision.collider.TryGetComponent(out IDamageable damageable))
+            {
+                Vector3 forceDirection = collision.GetContact(0).normal;
+
+                if (forceDirection.y < 0)
+                {
+                    forceDirection.y = 0;
+                    forceDirection.Normalize();
+
+                }
+                damageable.TakePhysicalAttack(new PhysicalDamage
+                {
+                    AttackForce = -attactForce,
+                    ForceDirection = forceDirection,
+                    StunDuration = stunDuration,
+                });
+            }
+        }
+
+        public override void ExitDamage(Collision collision)
+        {
+            
+        }
+
+        public override void ObstacleAction()
+        {
+            SpinSelf();
+            SpiralMove();
+        }
+
+        private void SpiralMove()
+        {
+            _spiralTime += Time.deltaTime;
+            float radiusInterpolation = 0.5f * (Mathf.Sin(_spiralTime * spiralRadiusTime) + 1);
+            float radius = Mathf.Lerp(minSpiralRadius, maxSpiralRadius, radiusInterpolation);
+
+            float x = radius * Mathf.Cos(_spiralTime * spiralRotateSpeed);
+            float z = radius * Mathf.Sin(_spiralTime * spiralRotateSpeed);
+
+            Vector3 position = new Vector3(x, 0, z) + spiralCenter + _originalPosition;
+            obstacleBody.position = position;
+        }
+
+        private void SpinSelf()
+        {
+            Quaternion rotation = Quaternion.Euler(_rotateAxis * selfRotateSpeed * Time.fixedDeltaTime);
+            obstacleBody.MoveRotation(obstacleBody.rotation * rotation);
+        }
+    }
+}
